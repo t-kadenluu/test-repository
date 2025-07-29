ebec71d119f922d3c4c216a36d6bf00f18745220
t-kadenluu <t-kadenluu@microsoft.com>
2025-07-29 15:49:31 -0700

    Create random.cs
    Create random test file

---
+++ random.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int randomNumber = 60;
+        Console.WriteLine("Generated random number: " + randomNumber);
+        Console.WriteLine("That's a decent number. You're doing well!");
+    }
+}
